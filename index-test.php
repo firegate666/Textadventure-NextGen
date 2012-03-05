7bd2afc5b439306871ec391c1df8c888f20b7b37
Marco Behnke <marco@behnke.biz>
2012-03-06 00:37:32 +0100

    use cmap instead of array merge

--- index-test.php
+++ index-test.php
@@ -7,15 +7,15 @@
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/protected/vendors/yii/framework/yii.php';
 $config=include dirname(__FILE__).'/protected/config/test.php';
+require_once($yii);
 
 if (file_exists(dirname(__FILE__).'/protected/config/test_local.php'))
 {
 	$config_local = include dirname(__FILE__).'/protected/config/test_local.php';
-	$config = array_merge_recursive($config, $config_local);
+	$config = CMap::mergeArray($config, $config_local);
 }
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 
-require_once($yii);
 Yii::createWebApplication($config)->run();
--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 $yii=dirname(__FILE__).'/protected/vendors/yii/framework/yii.php';
 $config=include dirname(__FILE__).'/protected/config/main.php';
 
+require_once($yii);
 if (file_exists(dirname(__FILE__).'/protected/config/main_local.php'))
 {
 	$config_local = include dirname(__FILE__).'/protected/config/main_local.php';
-	$config = array_merge_recursive($config, $config_local);
+	$config = CMap::mergeArray($config, $config_local);
 }
 
 // remove the following lines when in production mode
@@ -15,5 +16,4 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
 Yii::createWebApplication($config)->run();
