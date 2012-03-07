4d295084e515779f1de7469b573f1fb6c3f1ed50
Marco Behnke <marco@behnke.biz>
2012-03-07 22:33:33 +0100

    add missing local config

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -5,6 +5,12 @@ $yiit=dirname(__FILE__).'/../../../yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
+if (file_exists(dirname(__FILE__).'/../config/test_local.php'))
+{
+	$config_local = include dirname(__FILE__).'/../config/test_local.php';
+	$config = CMap::mergeArray($config, $config_local);
+}
+
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
 Yii::createWebApplication($config);
