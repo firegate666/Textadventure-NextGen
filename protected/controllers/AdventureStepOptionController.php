bcdf2cb73a94b5bf6db1007ed7edb1c20c55be8c
Marco Behnke <marco@behnke.biz>
2012-03-08 23:30:52 +0100

    update rules

--- protected/controllers/AdventureStepOptionController.php
+++ protected/controllers/AdventureStepOptionController.php
@@ -28,15 +28,15 @@ class AdventureStepOptionController extends Controller
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'users'=>array('@'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'expression'=>'$user->getState("isAdmin")',
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'expression'=>'$user->getState("isAdmin")',
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
