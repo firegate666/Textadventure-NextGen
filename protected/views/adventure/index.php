abeff48b924ef68f9496dd0e6a3be2f906cfd622
Marco Behnke <marco@behnke.biz>
2012-03-15 22:16:32 +0100

    re-format and change headline

--- protected/views/adventure/index.php
+++ protected/views/adventure/index.php
@@ -1,18 +1,18 @@
 <?php
-$this->breadcrumbs=array(
-	'Adventures',
+$this->breadcrumbs = array(
+	'Start adventuring',
 );
 
-$this->menu=array(
-	array('label'=>'Create Adventure', 'url'=>array('create')),
-	array('label'=>'Manage Adventure', 'url'=>array('admin')),
+$this->menu = array(
+	array('label' => 'Create Adventure', 'url' => array('create')),
+	array('label' => 'Manage Adventure', 'url' => array('admin')),
 );
 ?>
 
-<h1>Adventures</h1>
+<h1>Start adventuring</h1>
 
 <?php
 $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+	'dataProvider' => $dataProvider,
+	'itemView' => '_view',
 ));
