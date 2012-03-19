f42ff56ceb6cd1e1531bec104fd41912d23db0b4
Marco Behnke <marco@behnke.biz>
2012-03-19 08:56:17 +0100

    fix accessRules

--- protected/controllers/AdventureController.php
+++ protected/controllers/AdventureController.php
@@ -30,7 +30,7 @@ class AdventureController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions' => array('index', 'view', 'start', 'restart'),
+				'actions' => array('index', 'view', 'start', 'reset'),
 				'users' => array('@'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
