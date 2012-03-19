d9ada7e2fc4951d80a720b7be1b1a57ac2498db8
Marco Behnke <marco@behnke.biz>
2012-03-19 23:16:01 +0100

    add unittests for start, stop, state adventure

--- protected/tests/unit/AdventureTest.php
+++ protected/tests/unit/AdventureTest.php
@@ -16,6 +16,8 @@ class AdventureTest extends CDbTestCase
 
 		// adventure id is required
 		$this->assertTrue($model->hasErrors('adventureId'));
+		// state is required
+		$this->assertTrue($model->hasErrors('state'));
 		// bit gets an auto created value
 		$this->assertFalse(empty($model->adventureId));
 		$this->assertFalse(stripos($model->adventureId, ' '));
@@ -23,6 +25,72 @@ class AdventureTest extends CDbTestCase
 		// a second validation should auto accept the auto created value
 		$model->validate();
 		$this->assertFalse($model->hasErrors('adventureId'));
+		$this->assertTrue($model->hasErrors('state'));
+	}
 
+	public function testState()
+	{
+		$model = new Adventure();
+
+		$model->attributes = array(
+				'startDate' => null,
+				'stopDate' => null,
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertTrue($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => null,
+				'stopDate' => null,
+				'state' => Adventure::STATE_DRAFT,
+		);
+		$this->assertFalse($model->isRunning());
+	}
+
+	public function testStartStop()
+	{
+		$model = new Adventure();
+
+		$model->attributes = array(
+				'startDate' => date('Y-m-d', time()),
+				'stopDate' => null,
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertTrue($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => date('Y-m-d', time()-3600),
+				'stopDate' => null,
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertTrue($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => date('Y-m-d', time()+3600),
+				'stopDate' => null,
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertFalse($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => null,
+				'stopDate' => date('Y-m-d', time()+3600),
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertTrue($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => null,
+				'stopDate' => date('Y-m-d', time()),
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertFalse($model->isRunning());
+
+		$model->attributes = array(
+				'startDate' => null,
+				'stopDate' => date('Y-m-d', time()-3600),
+				'state' => Adventure::STATE_PUBLISHED,
+		);
+		$this->assertFalse($model->isRunning());
 	}
 }
