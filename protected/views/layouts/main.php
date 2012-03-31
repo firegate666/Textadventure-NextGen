ca5d752f263a799c529c6ef68608515e7c252efc
Marco Behnke <marco@behnke.biz>
2012-03-31 22:59:59 +0200

    extract piwik tracking code to partial

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -61,18 +61,9 @@
 
 </div><!-- page -->
 
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.virtualid.de/piwik/" : "http://piwik.virtualid.de/piwik/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 19);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://piwik.virtualid.de/piwik/piwik.php?idsite=19" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tracking Code -->
+<?php
+$this->renderPartial('//layouts/_piwik');
+?>
 
 </body>
 </html>
