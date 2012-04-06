02ce3edf75bc3aae18d9454d53ab879f12bc26b8
Marco Behnke <marco@behnke.biz>
2012-04-07 00:04:32 +0200

    switch layout to admin one column, there are no actions

--- protected/controllers/AdventureParticipationController.php
+++ protected/controllers/AdventureParticipationController.php
@@ -2,11 +2,7 @@
 
 class AdventureParticipationController extends Controller
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+	public $layout='//layouts/column1_admin';
 
 	/**
 	 * @return array action filters
