c907e0baebe7c6c449c0e2ae45234ada43911529
Marco Behnke <marco@behnke.biz>
2012-04-16 22:27:11 +0200

    extract generics to partials

--- protected/views/adventureParticipation/_view.php
+++ protected/views/adventureParticipation/_view.php
@@ -1,24 +1,8 @@
 <div class="view">
 
-	<b><?=CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?=CHtml::link(CHtml::encode($data->id), array('view', 'id' => $data->id)); ?>
-	<br />
-
-	<b><?=CHtml::encode($data->getAttributeLabel('createdAt')); ?>:</b>
-	<?=CHtml::encode($data->createdAt); ?>
-	<br />
-
-	<b><?=CHtml::encode($data->getAttributeLabel('changedAt')); ?>:</b>
-	<?=CHtml::encode($data->changedAt); ?>
-	<br />
-
-	<b><?=CHtml::encode($data->getAttributeLabel('createdBy')); ?>:</b>
-	<?=CHtml::encode($data->createdBy); ?>
-	<br />
-
-	<b><?=CHtml::encode($data->getAttributeLabel('changedBy')); ?>:</b>
-	<?=CHtml::encode($data->changedBy); ?>
-	<br />
+	<?php
+		$this->renderPartial('application.views.generics._view_metainfo', array('data' => $data));
+	?>
 
 	<b><?=CHtml::encode($data->getAttributeLabel('userId')); ?>:</b>
 	<?=CHtml::encode($data->userId); ?>
---
+++ protected/views/generics/_view_metainfo.php
@@ -0,0 +1,19 @@
+<b><?=CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+<?=CHtml::link(CHtml::encode($data->id), array('view', 'id' => $data->id)); ?>
+<br />
+
+<b><?=CHtml::encode($data->getAttributeLabel('createdAt')); ?>:</b>
+<?=CHtml::encode($data->createdAt); ?>
+<br />
+
+<b><?=CHtml::encode($data->getAttributeLabel('changedAt')); ?>:</b>
+<?=CHtml::encode($data->changedAt); ?>
+<br />
+
+<b><?=CHtml::encode($data->getAttributeLabel('createdBy')); ?>:</b>
+<?=CHtml::encode($data->createdBy); ?>
+<br />
+
+<b><?=CHtml::encode($data->getAttributeLabel('changedBy')); ?>:</b>
+<?=CHtml::encode($data->changedBy); ?>
+<br />
