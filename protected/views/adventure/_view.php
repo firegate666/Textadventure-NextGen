f6a18c9ec2f47183ce6abfb0e623d91a6f4cd7af
Marco Behnke <marco@behnke.biz>
2012-05-14 09:46:51 +0200

    indentation

--- protected/views/adventure/_view.php
+++ protected/views/adventure/_view.php
@@ -1,26 +1,26 @@
-	<div class="adventure view">
+<div class="adventure view">
 
-		<div class="description">
-			<h2><?=CHtml::encode($data->name);?></h2>
-			<blockquote>
-				<?=$this->widget('CMarkdown')->transform($data->description)?>
-			</blockquote>
-		</div>
-
-		<?php if ($data->hasStartingPoint() && $data->hasEndingPoint()): ?>
-			<div class="button button-play">
-				<?php if ($data->isRunning()): ?>
-					<?=CHtml::link($data->userInAdventure(Yii::app()->user->id)?'CONTINUE':'PLAY', array('view', 'id' => $data->id))?>
-				<?php else: ?>
-					<?=CHtml::link('Closed for now, please return later', '#')?>
-				<?php endif;?>
-			</div>
-		<?php else: ?>
-			<div class="button button-noplay">
-				<?=CHtml::link('Not open yet', null)?>
-			</div>
-		<?php endif;?>
-
-		<div class="clear"></div>
-
+	<div class="description">
+		<h2><?=CHtml::encode($data->name);?></h2>
+		<blockquote>
+			<?=$this->widget('CMarkdown')->transform($data->description)?>
+		</blockquote>
 	</div>
+
+	<?php if ($data->hasStartingPoint() && $data->hasEndingPoint()): ?>
+		<div class="button button-play">
+			<?php if ($data->isRunning()): ?>
+				<?=CHtml::link($data->userInAdventure(Yii::app()->user->id)?'CONTINUE':'PLAY', array('view', 'id' => $data->id))?>
+			<?php else: ?>
+				<?=CHtml::link('Closed for now, please return later', '#')?>
+			<?php endif;?>
+		</div>
+	<?php else: ?>
+		<div class="button button-noplay">
+			<?=CHtml::link('Not open yet', null)?>
+		</div>
+	<?php endif;?>
+
+	<div class="clear"></div>
+
+</div>
