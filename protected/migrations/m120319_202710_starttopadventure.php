66ffcb7bfb292d1d76521a251da6eee976c008fb
Marco Behnke <marco@behnke.biz>
2012-06-26 09:52:33 +0200

    fix column type

--- protected/migrations/m120319_202710_starttopadventure.php
+++ protected/migrations/m120319_202710_starttopadventure.php
@@ -5,7 +5,7 @@ class m120319_202710_starttopadventure extends CDbMigration
 	public function safeUp()
 	{
 		$this->addColumn('Adventure', 'startDate', 'date NOT NULL');
-		$this->addColumn('Adventure', 'startDate', 'stopDate NOT NULL');
+		$this->addColumn('Adventure', 'stopDate', 'date NOT NULL');
 		$this->addColumn('Adventure', 'state', 'integer NOT NULL DEFAULT 1');
 
 		$this->update('Adventure', array('state' => 2));
