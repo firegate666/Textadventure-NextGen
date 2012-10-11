140eab59cdb9a232eaf84093995a27523e5e3772
Marco Behnke <marco@behnke.biz>
2012-10-12 00:41:01 +0200

    fix privs

--- protected/controllers/IslandController.php
+++ protected/controllers/IslandController.php
@@ -44,11 +44,11 @@ class IslandController extends Controller
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'expression' => '$user->getState("isAdmin")',
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'expression' => '$user->getState("isAdmin")',
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
