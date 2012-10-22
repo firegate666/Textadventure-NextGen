e1cc3d0c653901d753fa2ce54ddffb4cccef972c
Marco Behnke <marco@behnke.biz>
2012-10-22 15:51:36 +0200

    adapt links to enter or continue world

--- protected/views/game/_world.php
+++ protected/views/game/_world.php
@@ -1,11 +1,22 @@
+<?php
+$action_text = 'Enter world';
+$action_name = 'enterWorld';
+
+if ($model->playerIsOnWorld(Yii::app()->user->id)) {
+	$action_text = 'Continue world';
+	$action_name = 'continueWorld';
+}
+?>
+
 <li>
 	<?=$model->name?>
-	(<?=CHtml::link(
-		'Play',
-		array(
-			'enterWorld',
-			'world_id' => $model->id
-		)
+	(
+		<?=CHtml::link(
+			$action_text,
+			array(
+				$action_name,
+				'world_id' => $model->id
+			)
+		)?>
 	)
-	?>)
 </li>
