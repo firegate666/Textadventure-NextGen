7fdaffe8496cc9f7fef09a4973d2740d69a43f17
Marco Behnke <marco@behnke.biz>
2016-06-10 10:08:24 +0200

    publish scripts from assets

--- protected/views/adventure/graph.php
+++ protected/views/adventure/graph.php
@@ -7,14 +7,23 @@ $this->breadcrumbs = array(
 
 $baseurl = Yii::app()->request->baseUrl;
 Yii::app()->clientScript->registerCoreScript('jquery');
-Yii::app()->clientScript->registerScriptFile($baseurl . '/public/vendors/dracula/vendor/raphael.js');
-Yii::app()->clientScript->registerScriptFile($baseurl . '/public/vendors/dracula/lib/dracula_graffle.js');
-Yii::app()->clientScript->registerScriptFile($baseurl . '/public/vendors/dracula/lib/dracula_graph.js');
-Yii::app()->clientScript->registerScriptFile($baseurl . '/public/js/main.js');
+
+$raphael_js = Yii::app()->assetManager->publish(__DIR__ . '/../../../htdocs/public/vendors/dracula/vendor/raphael.js');
+Yii::app()->clientScript->registerScriptFile($raphael_js);
+
+$dracula_graffle_js = Yii::app()->assetManager->publish(__DIR__ . '/../../../htdocs/public/vendors/dracula/lib/dracula_graffle.js');
+Yii::app()->clientScript->registerScriptFile($dracula_graffle_js);
+
+$dracula_graph_js = Yii::app()->assetManager->publish(__DIR__ . '/../../../htdocs/public/vendors/dracula/lib/dracula_graph.js');
+Yii::app()->clientScript->registerScriptFile($dracula_graph_js);
+
+$main_js = Yii::app()->assetManager->publish(__DIR__ . '/../../../htdocs/public/js/main.js');
+Yii::app()->clientScript->registerScriptFile($main_js);
+
+Yii::app()->clientScript->registerScript('adventure_graph_init', "
+Adventure.graph.init(" . json_encode($steps) . ", " . json_encode($edges_to_draw) . ").draw('canvas', $('#canvas').width(), 600);
+", CClientScript::POS_END);
 ?>
 
 <div id="canvas"></div>
 
-<script type="text/javascript">
-Adventure.graph.init(<?=json_encode($steps)?>, <?=json_encode($edges_to_draw) ?>).draw('canvas', $('#canvas').width(), 600);
-</script>
